06f17aeea6b039bb61f04cc1b63ab93afd28acf4
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2023-11-25 15:15:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Function to send the user's message to ChatGPT and display the response
+async function sendMessage() {
+    const inputText = document.getElementById('inputText').value;
+    if (inputText.trim() === '') return;
+
+    // Make a POST request to the ChatGPT API
+    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer YOUR_API_KEY' // Remember to replace YOUR_API_KEY with your actual ChatGPT API key
+        },
+        body: JSON.stringify({
+            prompt: inputText,
+            max_tokens: 150
+        })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        const chatContainer = document.getElementById('chatContainer');
+        const chatMessage = document.createElement('div');
+        chatMessage.classList.add('message', 'user');
+        chatMessage.textContent = inputText;
+        chatContainer.appendChild(chatMessage);
+
+        const botMessage = document.createElement('div');
+        botMessage.classList.add('message', 'bot');
+        botMessage.textContent = data.choices[0].text.trim();
+        chatContainer.appendChild(botMessage);
+    } else {
+        console.error('Error fetching ChatGPT response');
+    }
+}
